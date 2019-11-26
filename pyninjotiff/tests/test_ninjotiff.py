11a39aabcef6acda7c7e44a516c818407b389e24
Martin Raspaud <martin.raspaud@smhi.se>
2019-11-26 23:24:40 +0100

    Add unittests for the colormapped albedo and IR images

--- pyninjotiff/tests/test_ninjotiff.py
+++ pyninjotiff/tests/test_ninjotiff.py
@@ -43,6 +43,7 @@ class FakeImage(object):
         self.data = data
 
     def finalize(self, fill_value=None, dtype=None):
+        """Finalize the image."""
         if dtype is None:
             dtype = np.uint8
         if np.issubdtype(self.data.dtype, np.floating) and np.issubdtype(dtype, np.integer):
@@ -54,7 +55,10 @@ class FakeImage(object):
 
 
 class FakeArea(object):
+    """Fake area class."""
+
     def __init__(self, proj_dict, extent, y_size, x_size):
+        """Init the fake area."""
         self.proj_dict = proj_dict
         self.area_extent = extent
         self.x_size, self.y_size = x_size, y_size
@@ -63,7 +67,10 @@ class FakeArea(object):
 
 
 def test_write_bw():
-    """Test saving a BW image."""
+    """Test saving a BW image.
+
+    Reflectances.
+    """
     from pyninjotiff.ninjotiff import save
     from pyninjotiff.tifffile import TiffFile
 
@@ -454,3 +461,177 @@ def test_write_rgb_classified():
             np.testing.assert_allclose(res[:, :, idx], np.round(
                 np.nan_to_num(arr[idx, :, :]) * 255).astype(np.uint8))
         np.testing.assert_allclose(res[:, :, 3] == 0, np.isnan(arr[0, :, :]))
+
+
+def test_write_bw_colormap():
+    """Test saving a BW image with a colormap.
+
+    Albedo with a colormap.
+    """
+    from pyninjotiff.ninjotiff import save
+    from pyninjotiff.tifffile import TiffFile
+
+    area = FakeArea({'ellps': 'WGS84', 'lat_0': 90.0, 'lat_ts': 60.0, 'lon_0': 0.0, 'proj': 'stere'},
+                    (-1000000.0, -4500000.0, 2072000.0, -1428000.0),
+                    1024, 1024)
+    scale = 1.0 / 120
+    offset = 0.0
+    attrs = dict([('resolution', 1050),
+                  ('polarization', None),
+                  ('platform_name', 'NOAA-18'),
+                  ('sensor', 'avhrr-3'),
+                  ('units', '%'),
+                  ('name', '1'),
+                  ('level', None),
+                  ('modifiers', ()),
+                  ('wavelength', (10.3, 10.8, 11.3)),
+                  ('calibration', 'brightness_temperature'),
+                  ('start_time', TIME - datetime.timedelta(minutes=5)),
+                  ('end_time', TIME),
+                  ('area', area),
+                  ('ancillary_variables', []),
+                  ('enhancement_history', [{'offset': offset, 'scale': scale}])])
+
+    cm_vis = [0, 4095, 5887, 7167, 8191, 9215, 9983, 10751, 11519, 12287, 12799,
+              13567, 14079, 14847, 15359, 15871, 16383, 16895, 17407, 17919, 18175,
+              18687, 19199, 19711, 19967, 20479, 20735, 21247, 21503, 22015, 22271,
+              22783, 23039, 23551, 23807, 24063, 24575, 24831, 25087, 25599, 25855,
+              26111, 26367, 26879, 27135, 27391, 27647, 27903, 28415, 28671, 28927,
+              29183, 29439, 29695, 29951, 30207, 30463, 30975, 31231, 31487, 31743,
+              31999, 32255, 32511, 32767, 33023, 33279, 33535, 33791, 34047, 34303,
+              34559, 34559, 34815, 35071, 35327, 35583, 35839, 36095, 36351, 36607,
+              36863, 37119, 37119, 37375, 37631, 37887, 38143, 38399, 38655, 38655,
+              38911, 39167, 39423, 39679, 39935, 39935, 40191, 40447, 40703, 40959,
+              40959, 41215, 41471, 41727, 41983, 41983, 42239, 42495, 42751, 42751,
+              43007, 43263, 43519, 43519, 43775, 44031, 44287, 44287, 44543, 44799,
+              45055, 45055, 45311, 45567, 45823, 45823, 46079, 46335, 46335, 46591,
+              46847, 46847, 47103, 47359, 47615, 47615, 47871, 48127, 48127, 48383,
+              48639, 48639, 48895, 49151, 49151, 49407, 49663, 49663, 49919, 50175,
+              50175, 50431, 50687, 50687, 50943, 50943, 51199, 51455, 51455, 51711,
+              51967, 51967, 52223, 52223, 52479, 52735, 52735, 52991, 53247, 53247,
+              53503, 53503, 53759, 54015, 54015, 54271, 54271, 54527, 54783, 54783,
+              55039, 55039, 55295, 55551, 55551, 55807, 55807, 56063, 56319, 56319,
+              56575, 56575, 56831, 56831, 57087, 57343, 57343, 57599, 57599, 57855,
+              57855, 58111, 58367, 58367, 58623, 58623, 58879, 58879, 59135, 59135,
+              59391, 59647, 59647, 59903, 59903, 60159, 60159, 60415, 60415, 60671,
+              60671, 60927, 60927, 61183, 61439, 61439, 61695, 61695, 61951, 61951,
+              62207, 62207, 62463, 62463, 62719, 62719, 62975, 62975, 63231, 63231,
+              63487, 63487, 63743, 63743, 63999, 63999, 64255, 64255, 64511, 64511,
+              64767, 64767, 65023, 65023, 65279]
+
+    kwargs = {'ch_min_measurement_unit': np.array([0]),
+              'ch_max_measurement_unit': np.array([120]),
+              'compute': True, 'fill_value': None, 'sat_id': 6300014,
+              'chan_id': 100015, 'data_cat': 'PORN', 'data_source': 'SMHI',
+              'physic_unit': '%', 'nbits': 8, 'cmap': [cm_vis] * 3}
+
+    data = da.tile(da.repeat(da.arange(5, chunks=1024) / 4.0, 205)[:-1],
+                   1024).reshape((1, 1024, 1024))[:, :1024]
+    data = xr.DataArray(data, coords={'bands': ['L']}, dims=[
+                        'bands', 'y', 'x'], attrs=attrs)
+    img = FakeImage(data)
+    with tempfile.NamedTemporaryFile(delete=DELETE_FILES) as tmpfile:
+        filename = tmpfile.name
+        if not DELETE_FILES:
+            print(filename)
+        save(img, filename, data_is_scaled_01=True, **kwargs)
+        tif = TiffFile(filename)
+        page = tif[0]
+        res = page.asarray(colormapped=False).squeeze()
+        colormap = page.tags['color_map'].value
+
+        assert(len(colormap) == 768)
+        assert(np.allclose(colormap[:256], cm_vis))
+        assert(np.allclose(colormap[256:512], cm_vis))
+        assert(np.allclose(colormap[512:], cm_vis))
+        assert(np.allclose(res[0, ::205], np.array([1,  64, 128, 192, 255])))
+
+
+def test_write_ir_colormap():
+    """Test saving a IR image with a colormap.
+
+    IR with a colormap.
+    """
+    from pyninjotiff.ninjotiff import save
+    from pyninjotiff.tifffile import TiffFile
+
+    area = FakeArea({'ellps': 'WGS84', 'lat_0': 90.0, 'lat_ts': 60.0, 'lon_0': 0.0, 'proj': 'stere'},
+                    (-1000000.0, -4500000.0, 2072000.0, -1428000.0),
+                    1024, 1024)
+    scale = 1.0 / 120
+    offset = 70.0 / 120
+    attrs = dict([('resolution', 1050),
+                  ('polarization', None),
+                  ('platform_name', 'NOAA-18'),
+                  ('sensor', 'avhrr-3'),
+                  ('units', 'K'),
+                  ('name', '4'),
+                  ('level', None),
+                  ('modifiers', ()),
+                  ('wavelength', (10.3, 10.8, 11.3)),
+                  ('calibration', 'brightness_temperature'),
+                  ('start_time', TIME - datetime.timedelta(minutes=15)),
+                  ('end_time', TIME - datetime.timedelta(minutes=10)),
+                  ('area', area),
+                  ('ancillary_variables', []),
+                  ('enhancement_history', [{'offset': offset, 'scale': scale}])])
+
+    ir_map = [255, 1535, 2559, 3327, 4095, 4863, 5375, 5887, 6399,
+              6911, 7423, 7935, 8447, 8959, 9471, 9983, 10239, 10751,
+              11263, 11519, 12031, 12287, 12799, 13055, 13567, 13823,
+              14335, 14591, 14847, 15359, 15615, 16127, 16383, 16639,
+              17151, 17407, 17663, 17919, 18431, 18687, 18943, 19199,
+              19711, 19967, 20223, 20479, 20735, 21247, 21503, 21759,
+              22015, 22271, 22527, 22783, 23295, 23551, 23807, 24063,
+              24319, 24575, 24831, 25087, 25343, 25599, 25855, 26367,
+              26623, 26879, 27135, 27391, 27647, 27903, 28159, 28415,
+              28671, 28927, 29183, 29439, 29695, 29951, 30207, 30463,
+              30719, 30975, 31231, 31487, 31743, 31999, 31999, 32255,
+              32511, 32767, 33023, 33279, 33535, 33791, 34047, 34303,
+              34559, 34815, 35071, 35327, 35327, 35583, 35839, 36095,
+              36351, 36607, 36863, 37119, 37375, 37375, 37631, 37887,
+              38143, 38399, 38655, 38911, 39167, 39167, 39423, 39679,
+              39935, 40191, 40447, 40703, 40703, 40959, 41215, 41471,
+              41727, 41983, 41983, 42239, 42495, 42751, 43007, 43263,
+              43263, 43519, 43775, 44031, 44287, 44287, 44543, 44799,
+              45055, 45311, 45311, 45567, 45823, 46079, 46335, 46335,
+              46591, 46847, 47103, 47359, 47359, 47615, 47871, 48127,
+              48127, 48383, 48639, 48895, 49151, 49151, 49407, 49663,
+              49919, 49919, 50175, 50431, 50687, 50687, 50943, 51199,
+              51455, 51455, 51711, 51967, 52223, 52223, 52479, 52735,
+              52991, 52991, 53247, 53503, 53759, 53759, 54015, 54271,
+              54527, 54527, 54783, 55039, 55039, 55295, 55551, 55807,
+              55807, 56063, 56319, 56319, 56575, 56831, 57087, 57087,
+              57343, 57599, 57599, 57855, 58111, 58367, 58367, 58623,
+              58879, 58879, 59135, 59391, 59391, 59647, 59903, 60159,
+              60159, 60415, 60671, 60671, 60927, 61183, 61183, 61439,
+              61695, 61695, 61951, 62207, 62463, 62463, 62719, 62975,
+              62975, 63231, 63487, 63487, 63743, 63999, 63999, 64255,
+              64511, 64511, 64767, 65023, 65023, 65279]
+
+    kwargs = {'ch_min_measurement_unit': np.array([-70]),
+              'ch_max_measurement_unit': np.array([50]),
+              'compute': True, 'fill_value': None, 'sat_id': 6300014,
+              'chan_id': 900015, 'data_cat': 'PORN', 'data_source': 'SMHI',
+              'physic_unit': 'C', 'nbits': 8, 'cmap': [ir_map] * 3}
+
+    data = da.tile(da.repeat(da.arange(5, chunks=1024) / 4.0, 205)[:-1],
+                   1024).reshape((1, 1024, 1024))[:, :1024]
+    data = xr.DataArray(data, coords={'bands': ['L']}, dims=[
+                        'bands', 'y', 'x'], attrs=attrs)
+    img = FakeImage(data)
+    with tempfile.NamedTemporaryFile(delete=DELETE_FILES) as tmpfile:
+        filename = tmpfile.name
+        if not DELETE_FILES:
+            print(filename)
+        save(img, filename, data_is_scaled_01=True, **kwargs)
+        tif = TiffFile(filename)
+        page = tif[0]
+        res = page.asarray(colormapped=False).squeeze()
+        colormap = page.tags['color_map'].value
+
+        assert(len(colormap) == 768)
+        assert(np.allclose(colormap[:256], ir_map))
+        assert(np.allclose(colormap[256:512], ir_map))
+        assert(np.allclose(colormap[512:], ir_map))
+        assert(np.allclose(res[0, ::205], np.array([1,  64, 128, 192, 255])))
